ab32a262dfd923acceb73d34042058a3ec82169d
siuhim <siuhim.tang@gmail.com>
2014-03-18 12:07:44 -0400

    Midterm_1

--- iOS midterm/MyScene.m
+++ iOS midterm/MyScene.m
@@ -10,20 +10,45 @@
 
 @implementation MyScene
 
+{
+    SKSpriteNode *_charater;
+    SKSpriteNode *_block;
+    SKSpriteNode *_flag;
+    
+}
+
+
+
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        SKSpriteNode* background = [SKSpriteNode spriteNodeWithImageNamed:@"b1.png"];
+        background.position = CGPointMake(self.frame.size.width, self.frame.size.height);
+        [self addChild:background];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
         
-        [self addChild:myLabel];
+        _block = [SKSpriteNode spriteNodeWithImageNamed: @"c2.png"];
+        _block.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+        _block.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_block.size];
+       [_block.physicsBody setDynamic:NO];
+        [self addChild:_block];
+        
+        _flag = [SKSpriteNode spriteNodeWithImageNamed: @"flag.png"];
+        _flag.position = CGPointMake(self.frame.size.width/4, self.frame.size.height/2);
+        _flag.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_flag.size];
+        [_flag.physicsBody setDynamic:NO];
+        [self addChild:_flag];
+        
+        _charater = [SKSpriteNode spriteNodeWithImageNamed: @"c1.png"];
+        _charater.position = CGPointMake(self.frame.size.width/3, self.frame.size.height/3);
+        _charater.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_charater.size];
+        [_charater.physicsBody setDynamic:NO];
+        [self addChild:_charater];
+        
+        
+        
     }
     return self;
 }
@@ -31,19 +56,36 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
+   for (UITouch *touch in touches) {
+       CGPoint location = [touch locationInNode:self];
+        [_charater setPosition:location];
+    
+    
+        
+    }
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
+        [_charater setPosition:location];
         
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
     }
+}
+
+-(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event  {
+    
+
+    
+}
+
+-(void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
+//    
+//    if (!_mySquare1.physicsBody.dynamic) {
+//        [_mySquare1.physicsBody setDynamic:YES];
+//    }
+
+    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
